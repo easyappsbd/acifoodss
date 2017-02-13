34badc82e88017ca294f423e173905eeb6292204
easyappsbd <aciltdmis@gmail.com>
2017-02-13 19:07:03 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -16,7 +16,54 @@
 	</style>
 </head>
 
-<body><?php
+<body>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1761709417490285',
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+
+    // ADD ADDITIONAL FACEBOOK CODE HERE
+	function onLogin(response) {
+	  if (response.status == 'connected') {
+		FB.api('/me?fields=first_name', function(data) {
+		  var welcomeBlock = document.getElementById('fb-welcome');
+		  welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+		});
+	  }
+	}
+
+	FB.getLoginStatus(function(response) {
+	  // Check login status on load, and if the user is
+	  // already logged in, go directly to the welcome message.
+	  if (response.status == 'connected') {
+		onLogin(response);
+	  } else {
+		// Otherwise, show Login dialog first.
+		FB.login(function(response) {
+		  onLogin(response);
+		}, {scope: 'user_friends, email'});
+	  }
+	});
+	
+	
+	
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+<h1 id="fb-welcome"></h1>
+
+
+<?php
 $Quiz = array(
 	array(
 		"Question" => "What is your preferred activity for hangout?",
@@ -137,5 +184,7 @@ else{
 
 	print "<form action=\"./\" method=\"POST\">" . implode(null, $HTML) . "<button name=\"btnSubmit\" value=\"1\">Submit</button></button></form>";
 }
-?></body>
+?>
+
+</body>
 </html>
